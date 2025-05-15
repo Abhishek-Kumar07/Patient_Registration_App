080b355d9b313a63b33ea26d9e5d8a23778f8aa1
Abhishek-Kumar07 <a3@bmsit.in>
2025-05-16 01:08:05 +0530

    Update vite.config.js
    Update

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['@electric-sql/pglite'], // force pre-bundling so Vite can handle it
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ['@electric-sql/pglite'], // make sure nothing is marked external, so it bundles everything
+//     },
+//   },
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@electric-sql/pglite'], // force pre-bundling so Vite can handle it
-  },
-  build: {
-    rollupOptions: {
-      external: ['@electric-sql/pglite'], // make sure nothing is marked external, so it bundles everything
-    },
+    include: ['@electric-sql/pglite'],
   },
 })
-
-
-
 
