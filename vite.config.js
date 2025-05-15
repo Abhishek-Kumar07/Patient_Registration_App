b2fca467f95c79e2f458b1b342a4f732ac407d9b
Abhishek-Kumar07 <a3@bmsit.in>
2025-05-16 01:37:55 +0530

    Modify vite.config.js
    Modify file

--- vite.config.js
+++ vite.config.js
@@ -18,20 +18,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     include: ['@electric-sql/pglite']
-//   }
-// })
-
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@electric-sql/pglite'],
-  },
-  build: {}
+    include: ['@electric-sql/pglite']
+  }
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['@electric-sql/pglite'],
+//   },
+//   build: {}
+// })
 
 
 
